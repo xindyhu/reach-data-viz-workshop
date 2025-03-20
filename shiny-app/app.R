dc00a53b2d3bc208feadfdc1a53301c08594d594
Cindy Hu <xindy.hu@gmail.com>
2025-03-19 21:21:53 -0400

    feat: change folder structure

--- shiny-app/app.R
+++ shiny-app/app.R
@@ -5,6 +5,9 @@ library(ggplot2)
 library(DT)
 library(plotly)
 library(viridis)
+
+options(shiny.reactlog = TRUE)
+
 coffee_ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv')
 
 coffee_clean <- coffee_ratings %>%
